007635c883d43e638d19afac45ee4c4ff90621e6
Sagar-bangari <153585123+Sagar-bangari@users.noreply.github.com>
2024-01-24 23:58:15 +0530

    Add files via upload
    LAB - 6 uploaded

---
+++ LAB - 6 packages/CIE/Studentmain.java
@@ -0,0 +1,23 @@
+package CIE;
+import SEE.Externals;
+public class Studentmain {
+    public static void main(String[] args) {
+        int n=1;
+        Externals finalmarks[]=new Externals[n];
+        for (int i = 0; i < n; i++) 
+        {
+            finalmarks[i]=new Externals();
+            finalmarks[i].inputstudentdetails();
+            System.out.println("Enter CIE marks:");
+            finalmarks[i].inputcie();
+            System.out.println("Enter SEE marks");
+            finalmarks[i].inputsee();
+        }
+        System.out.println("Displaying data:");
+        for(int i=0;i<n;i++)
+        {
+            finalmarks[i].cfmarks();
+            finalmarks[i].displayfmarks();
+        }
+    }
+}
